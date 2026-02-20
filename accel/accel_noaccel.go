64ac4ea502b6ac132d7dd4fa248584fc326be72b
Arianna Method <theariannamethod@gmail.com>
2026-02-20 13:28:31 +0200

    Delete accel directory

--- accel/accel_noaccel.go
+++
@@ -1,9 +0,0 @@
-//go:build !(cgo && (darwin || linux))
-
-package accel
-
-func Dgemv(M, N int, A, x, y []float64) {}
-func Dgemm(M, N, K int, alpha float64, A, B []float64, beta float64, C_ []float64) {}
-func Ddot(N int, x, y []float64) float64 { return 0 }
-
-const HasAccel = false
